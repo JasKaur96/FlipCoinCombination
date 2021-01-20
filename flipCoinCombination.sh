1c4a6f6b23a39f78fb2a53a84f8b744d149da45a
JasKaur96 <jaspreetflora7@gmail.com>
2021-01-20 17:21:00 +0530

    UC2: SingletPercentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,28 @@
-  
+#!/bin/bash -x
 
- coin=$((RANDOM%2))
-if [[ $coin -eq 1 ]]
+flips=0
+
+while [[ $flips -lt 10 ]]
+do
+   coin=$((RANDOM%2))
+   if [[ $coin -eq 1 ]]
         then
         echo "Heads"
+	h=$(($h+1))
         else
         echo "Tails"
-fi
-        
+	t=$(($t+1))
+	
+   fi
+
+   flips=$(($flips+1))
+	
+done     
+echo "$flips flips $h h $t h"
+  pHead=`awk "BEGIN { print ($h/$flips)*100 }"`
+  
+  percTail=`awk "BEGIN { print ($t/$flips)*100 }"`
+
+	echo "$pHead% head"
+	echo "$percTail% Tail"
+
